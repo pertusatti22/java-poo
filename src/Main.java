41df61dcc4c5f6c8833b24cbb3e338c3a385cfec
pertusatti22 <pertusatti22@gmail.com>
2023-01-12 12:03:18 -0300

    Release 1.0.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import domain.Course;
-import domain.Mentorship;
+import domain.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,48 @@ public class Main {
         mentoriaGIT.setDescription("Melhorando seus estudos a partir do versionamento de código");
         mentoriaGIT.setDate(LocalDate.now());
 
-
+        System.out.println("Lista de Conteúdos");
         System.out.println(courseJava);
         System.out.println(courseAngular);
         System.out.println(mentoriaGIT);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setTitle("Bootcamp Java Developer");
+        bootcamp.setDescription("Bootcamp para estudantes de Java");
+        bootcamp.getContents().add(courseJava);
+        bootcamp.getContents().add(mentoriaGIT);
+        bootcamp.getContents().add(courseAngular);
+
+        Student student = new Student();
+        student.setName("Jack");
+
+        System.out.println("\n-- " + student.getName() + " start XP: " + student.xpCalculate() + " --\n");
+        System.out.println("-- Conteúdos inscritos antes do bootcamp --");
+        System.out.println(student.getSubscribedContents());
+
+        student.bootcampSignUp(bootcamp);
+
+        System.out.println("\n-- Conteúdos inscritos depois do bootcamp --");
+
+        for(Content c: student.getSubscribedContents()){
+            System.out.println(c);
+        }
+
+        System.out.println("\n-- Conteúdos concluídos antes de progredir --");
+        System.out.println(student.getDoneContents());
+
+        student.leveling();
+
+        System.out.println("\n-- Conteúdos inscritos depois do progredir --");
+        for(Content c: student.getSubscribedContents()){
+            System.out.println(c);
+        }
+
+        System.out.println("\n-- Conteúdos concluídos depois de progredir --");
+        for(Content c: student.getDoneContents()){
+            System.out.println(c);
+        }
+
+        System.out.println("\n-- " + student.getName() + " final XP: " + student.xpCalculate() + "--");
     }
 }
